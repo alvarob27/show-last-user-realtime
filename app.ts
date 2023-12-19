cd33534fcb1d3d788ba8b290d1066ee2d9481658
alvarob27 <alvarobarcena27@gmail.com>
2023-12-19 17:30:58 +0100

    Remove commented code and unused imports

--- app.ts
+++ app.ts
@@ -10,58 +10,15 @@ import { streamSSE } from "https://deno.land/x/hono@v3.11.8/helper/streaming/ind
 // Creamos la aplicación con Hono
 const app = new Hono();
 app.use(cors());
+
 //Abrimos conexión con la base de datos
 const db = await Deno.openKv();
+
 // Creamos un contador
 let i = 0;
 
 // Creamos una ruta para servir el index.html
 app.get("/", serveStatic({ path: "./index.html" }));
-
-// // Creamos una ruta para el stream de datos
-// app.get("/counter", (c) => {
-//   // Creamos el stream de datos
-//   return streamSSE(c, async (stream) => {
-//     // Creamos un bucle infinito para servir la hora actual cada segundo
-//     while (true) {
-//       // Creamos un watcher u observador para la clave visits de la base de datos
-//       // El watcher recibe un array de claves a observar por eso lo pasamos como un array de un elemento
-//       const watcher = db.watch([["visits"]]);
-//       for await (const entry of watcher) {
-//         // Recuperamos el valor de la clave visits ya que está en el primer elemento del array
-//         const { value } = entry[0];
-
-//         if (value !== null) {
-//           // Escribimos el mensaje en el stream e incrementamos el contador
-//           await stream.writeSSE({
-//             data: Number(value).toString(),
-//             event: "update",
-//             id: String(i++),
-//           });
-//         }
-//       }
-//       // const { value } = await db.get(["visits"]);
-//       // await stream.writeSSE({ data: Number(value).toString(), event: 'update', id: String(i++) });
-//       // await stream.sleep(1000);
-//       // // Creamos el mensaje
-//       // const message = `Son las ${new Date().toLocaleTimeString()}`;
-//       // // Escribimos el mensaje en el stream e incrementamos el contador
-//       // await stream.writeSSE({ data: message, event: 'update', id: String(i++) });
-//       // // Dormimos el stream durante un segundo
-//       // await stream.sleep(1000);
-//     }
-//   });
-// });
-
-// // Creamos una ruta para aumentar el contador en la base de datos
-// app.post("/counter", async (c) => {
-//   // Hacemos una operación atómica para que no se solapen las operaciones
-//   // Utilizamos la función sum para sumar 1 al valor de la clave "visits"
-//   // Hacemos un commit para guardar los cambios
-//   await db.atomic().sum(["visits"], 1n).commit();
-//   // Devolvemos un mensaje de OK
-//   return c.json({ message: "OK" });
-// });
 
 interface LastVisit {
   city: string;
